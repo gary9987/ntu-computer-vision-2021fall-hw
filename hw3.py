fd67de45863671056e5d8208b1a2bae86c213acd
Guan-Ying Chen <s310068@shsh.tw>
2021-10-18 21:40:11 +0800

    hw3 finish 10/18

---
+++ hw3.py
@@ -0,0 +1,48 @@
+from hw2 import calculateDestribution
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def devideBy3(img):
+
+    new_img = np.zeros((img.shape[0], img.shape[1]))
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            new_img[i][j] = int(img[i][j] / 3)
+
+    return new_img
+
+
+def histEqualization(img):
+
+    new_img = np.zeros((img.shape[0], img.shape[1]))
+
+    hist = calculateDestribution(img)
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            new_img[i][j] = 255*sum(hist[:int(img[i][j])]) / (img.shape[0] * img.shape[1])
+
+    return new_img
+
+
+if __name__ == '__main__':
+    img = cv2.imread('lena.bmp', cv2.IMREAD_GRAYSCALE)
+
+    b = devideBy3(img)
+    cv2.imwrite('./output/hw3/b.bmp', b)
+    b_hist = calculateDestribution(b)
+
+    plt.bar(list(range(0, 256)), b_hist)
+    plt.savefig('./output/hw3/b_hist.png')
+
+    c = histEqualization(img)
+    cv2.imwrite('./output/hw3/c.bmp', c)
+
+    c_hist = calculateDestribution(c)
+    plt.close()
+    plt.bar(list(range(0, 256)), c_hist)
+    plt.savefig('./output/hw3/c_hist.png')
+

8e93496420f88d33c3dc49a8f50edd52c5ac1027
Guan-Ying Chen <s310068@shsh.tw>
2021-10-22 00:18:46 +0800

    fin erosion, opening, closing

--- hw4.py
+++ hw4.py
@@ -24,15 +24,14 @@ def scaleBackTo0_255(img):
     return new_img
 
 
-def maskUnion(new_img, img, r, c, mask):
-    if (img[r][c] != 0):
-        for i in range(-2, 3):
-            for j in range(-2, 3):
-                if (r + i >= 0 and r + i < img.shape[0] and c + j >= 0 and c + j < img.shape[1]):
-                    new_img[r + i][c + j] = new_img[r + i][c + j] or mask[2 + i][2 + j]
-
-
 def dilation(img, mask):
+    def maskUnion(new_img, img, r, c, mask):
+        if (img[r][c] != 0):
+            for i in range(-2, 3):
+                for j in range(-2, 3):
+                    if (r + i >= 0 and r + i < img.shape[0] and c + j >= 0 and c + j < img.shape[1]):
+                        new_img[r + i][c + j] = new_img[r + i][c + j] or mask[2 + i][2 + j]
+
     new_img = np.zeros((img.shape[0], img.shape[1]))
 
     for row in range(img.shape[0]):
@@ -41,6 +40,35 @@ def dilation(img, mask):
 
     return new_img
 
+
+def maskMatch(img, r, c, mask):
+
+    for i in range(-2, 3):
+        for j in range(-2, 3):
+            if (r + i >= 0 and r + i < img.shape[0] and c + j >= 0 and c + j < img.shape[1]):
+                if mask[2 + i][2 + j] == 1 and img[r + i][c + j] == 0:
+                    return False
+
+    return True
+
+def erosion(img, mask):
+
+    new_img = np.zeros((img.shape[0], img.shape[1]))
+
+    for row in range(img.shape[0]):
+        for col in range(img.shape[1]):
+            if maskMatch(img, row, col, mask):
+                new_img[row][col] = 1
+
+    return new_img
+
+def opening(img, mask):
+
+    return dilation(erosion(img, mask), mask)
+
+def closing(img, mask):
+
+    return erosion(dilation(img, mask), mask)
 
 if __name__ == '__main__':
     img = cv2.imread('lena.bmp', cv2.IMREAD_GRAYSCALE)
@@ -51,6 +79,15 @@ if __name__ == '__main__':
             [0, 1, 1, 1, 0]]
 
     b_img = scaleTo0_1(binarilize(img))
+    '''
     a = scaleBackTo0_255(dilation(b_img, mask))
     cv2.imwrite('./output/hw4/a.bmp', a)
 
+    b = scaleBackTo0_255(erosion(b_img, mask))
+    cv2.imwrite('./output/hw4/b.bmp', b)
+    '''
+    c = scaleBackTo0_255(opening(b_img, mask))
+    cv2.imwrite('./output/hw4/c.bmp', c)
+
+    d = scaleBackTo0_255(closing(b_img, mask))
+    cv2.imwrite('./output/hw4/d.bmp', d)
